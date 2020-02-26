33f2b28c294a3db13f7bc07023471f2404677bb1
siem <1138793164@qq.com>
2020-02-26 18:13:40 +0800

    first push

---
+++ src/common/const.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: huangzibin
+ * @Date: 2020-02-26 17:58:07
+ * @Last Modified by: huangzibin
+ * @Last Modified time: 2020-02-26 17:58:10
+ */
---
+++ src/views/home/Home.vue
@@ -0,0 +1,25 @@
+/*
+ * @Author: huangzibin
+ * @Date: 2020-02-26 17:58:14
+ * @Last Modified by: huangzibin
+ * @Last Modified time: 2020-02-26 17:58:58
+ */
+<template>
+  <div class="container">
+    <h1>首页</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Home",
+    data() {
+      return {
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
